c5b1e2dcd08ce330a0bfe257537bf50dbc98c67b
Seo sangmin <139242512+smpringles24@users.noreply.github.com>
2023-11-12 10:48:29 +0900

    :sparkles: Get some data from backend, and build app (#4)
    .

---
+++ lib/ui/memory/memory_view_model.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:sm_bar_master_frontend/data/model/memory_model.dart';
+import 'package:sm_bar_master_frontend/data/datasource/remote_datasource.dart';
+
+class MemoryViewModel with ChangeNotifier {
+  MemoryViewModel() {
+    _memoryPreviewImages = fetchAllAlbumPreviewImage();
+  }
+
+  Future<List<AlbumPreviewImageModel>>? _memoryPreviewImages;
+
+  Future<List<AlbumPreviewImageModel>>? get memoryPreviewImages =>
+      _memoryPreviewImages;
+
+  Future<void> fetchdata() async {
+    _memoryPreviewImages = fetchAllAlbumPreviewImage();
+    await _memoryPreviewImages; // 데이터 로딩 완료 대기
+    notifyListeners(); // 데이터 변경 알림
+  }
+}
