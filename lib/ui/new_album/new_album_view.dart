2f74496c70bb88bc6e14bed712c940cb2900f2e8
smpringles24 <smpringles24@gmail.com>
2023-11-12 14:13:04 +0900

    :sparkles: Implement create album page with some funny ui.
    코드 정리 필요

--- lib/ui/new_album/new_album_view.dart
+++ lib/ui/new_album/new_album_view.dart
@@ -14,71 +14,84 @@ class NewAlbumView extends StatelessWidget {
     print(newAlbumViewModel.nowSelected);
 
     return Scaffold(
-        backgroundColor: Colors.black87,
-        body: Row(
-          children: [
-            Container(
-              color: const Color.fromARGB(99, 255, 255, 255),
-              width: 240,
-              child: const Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text("앨범 제목", style: TextStyle(fontSize: 25)),
-                  Text("날짜", style: TextStyle(fontSize: 25)),
-                  Text("배경 색", style: TextStyle(fontSize: 25)),
-                  Text("CD 이미지", style: TextStyle(fontSize: 25)),
-                  Text("칵테일 이름", style: TextStyle(fontSize: 25)),
-                  Text("한줄평", style: TextStyle(fontSize: 25)),
-                ],
-              ),
+      backgroundColor: Colors.black87,
+      body: Row(
+        children: [
+          Container(
+            color: const Color.fromARGB(99, 255, 255, 255),
+            width: 240,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                newAlbumViewModel.nowSelected == selectedOption.date
+                    ? const Text("날짜", style: TextStyle(fontSize: 45))
+                    : const Text("날짜", style: TextStyle(fontSize: 25)),
+                newAlbumViewModel.nowSelected == selectedOption.albumTitle
+                    ? const Text("앨범 제목", style: TextStyle(fontSize: 45))
+                    : const Text("앨범 제목", style: TextStyle(fontSize: 25)),
+                newAlbumViewModel.nowSelected == selectedOption.backgroundColor
+                    ? const Text("배경 색", style: TextStyle(fontSize: 45))
+                    : const Text("배경 색", style: TextStyle(fontSize: 25)),
+                newAlbumViewModel.nowSelected == selectedOption.cdImage
+                    ? const Text("CD 이미지", style: TextStyle(fontSize: 45))
+                    : const Text("CD 이미지", style: TextStyle(fontSize: 25)),
+                newAlbumViewModel.nowSelected == selectedOption.cocktailName
+                    ? const Text("칵테일 이름", style: TextStyle(fontSize: 45))
+                    : const Text("칵테일 이름", style: TextStyle(fontSize: 25)),
+                newAlbumViewModel.nowSelected == selectedOption.review
+                    ? const Text("한줄평", style: TextStyle(fontSize: 45))
+                    : const Text("한줄평", style: TextStyle(fontSize: 25)),
+              ],
             ),
-            Expanded(
-              child: Column(
-                children: [
-                  Expanded(
-                    flex: 1,
-                    child: Container(
-                      width: double.infinity,
-                      color: const Color(0xFF81B71A),
-                      child: const Center(
-                        child: Text(
-                          "새로운 앨범 추가",
-                          style: TextStyle(
-                              color: Color.fromARGB(255, 0, 0, 0),
-                              fontSize: 45,
-                              fontWeight: FontWeight.w700),
-                        ),
+          ),
+          Expanded(
+            child: Column(
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    width: double.infinity,
+                    color: const Color(0xFF81B71A),
+                    child: const Center(
+                      child: Text(
+                        "새로운 앨범 추가",
+                        style: TextStyle(
+                            color: Color.fromARGB(255, 0, 0, 0),
+                            fontSize: 45,
+                            fontWeight: FontWeight.w700),
                       ),
                     ),
                   ),
-                  Expanded(
-                    flex: 6,
-                    child: Stack(children: [
-                      _AlbumEditPreview(newAlbumViewModel: newAlbumViewModel),
-                    ]),
+                ),
+                Expanded(
+                  flex: 6,
+                  child: Stack(children: [
+                    _AlbumEditPreview(newAlbumViewModel: newAlbumViewModel),
+                  ]),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: TextButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: const Text(
+                          "추가하기!",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 25,
+                              fontWeight: FontWeight.w700),
+                        )),
                   ),
-                  Expanded(
-                    flex: 1,
-                    child: SizedBox(
-                      width: double.infinity,
-                      child: TextButton(
-                          onPressed: () {
-                            Navigator.pop(context);
-                          },
-                          child: const Text(
-                            "추가하기!",
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 25,
-                                fontWeight: FontWeight.w700),
-                          )),
-                    ),
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
-          ],
-        ));
+          ),
+        ],
+      ),
+    );
   }
 }
 
@@ -103,29 +116,54 @@ class _AlbumEditPreview extends StatelessWidget {
         children: [
           Padding(
             padding: const EdgeInsets.only(bottom: 12),
-            child: Text(
-              '$date - $albumTitle',
-              style: const TextStyle(color: Colors.white, fontSize: 35),
+            child: Row(
+              children: [
+                MouseRegion(
+                  onEnter: (_) => newAlbumViewModel
+                      .changeSelectedOption(selectedOption.date),
+                  child: Text(
+                    date,
+                    style: const TextStyle(color: Colors.white, fontSize: 35),
+                  ),
+                ),
+                const Text(
+                  ' - ',
+                  style: TextStyle(color: Colors.white, fontSize: 35),
+                ),
+                MouseRegion(
+                  onEnter: (_) => newAlbumViewModel
+                      .changeSelectedOption(selectedOption.albumTitle),
+                  child: Text(
+                    albumTitle,
+                    style: const TextStyle(color: Colors.white, fontSize: 35),
+                  ),
+                ),
+              ],
             ),
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Container(
-                width: 600,
-                height: 600,
-                color: Color(int.parse(backgroundColor)),
-                child: MouseRegion(
-                  onEnter: (_) => newAlbumViewModel.changeSelectedOption(selectedOption.cdImage),
+              MouseRegion(
+                onEnter: (_) => newAlbumViewModel
+                    .changeSelectedOption(selectedOption.backgroundColor),
+                child: Container(
+                  width: 600,
+                  height: 600,
+                  color: Color(int.parse(backgroundColor)),
                   child: Stack(
                     alignment: Alignment.center,
                     children: <Widget>[
                       ClipOval(
-                        child: Image.asset(
-                          cdImage,
-                          fit: BoxFit.cover,
-                          width: 550,
-                          height: 550,
+                        child: MouseRegion(
+                          onEnter: (_) => newAlbumViewModel
+                              .changeSelectedOption(selectedOption.cdImage),
+                          child: Image.asset(
+                            cdImage,
+                            fit: BoxFit.cover,
+                            width: 550,
+                            height: 550,
+                          ),
                         ),
                       ),
                       Container(
@@ -150,36 +188,53 @@ class _AlbumEditPreview extends StatelessWidget {
                   ),
                 ),
               ),
-              Container(
-                height: 600,
-                width: 20,
-                color: const Color.fromARGB(255, 44, 57, 63),
+              MouseRegion(
+                onEnter: (_) => newAlbumViewModel
+                    .changeSelectedOption(selectedOption.backgroundColor),
+                child: Container(
+                  height: 600,
+                  width: 20,
+                  color: const Color.fromARGB(255, 44, 57, 63),
+                ),
               ),
-              Container(
-                height: 600,
-                width: 600,
-                color: Color(int.parse(backgroundColor)),
-                child: Padding(
-                  padding: const EdgeInsets.all(45),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(bottom: 15),
-                        child: Text(
-                          cocktailName,
-                          style: const TextStyle(
-                              color: Colors.white,
-                              fontSize: 32,
-                              fontWeight: FontWeight.w800),
+              MouseRegion(
+                onEnter: (_) => newAlbumViewModel
+                    .changeSelectedOption(selectedOption.backgroundColor),
+                child: Container(
+                  height: 600,
+                  width: 600,
+                  color: Color(int.parse(backgroundColor)),
+                  child: Padding(
+                    padding: const EdgeInsets.all(45),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 15),
+                          child: MouseRegion(
+                            onEnter: (_) =>
+                                newAlbumViewModel.changeSelectedOption(
+                                    selectedOption.cocktailName),
+                            child: Text(
+                              cocktailName,
+                              style: const TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 32,
+                                  fontWeight: FontWeight.w800),
+                            ),
+                          ),
                         ),
-                      ),
-                      Text(
-                        comment,
-                        style:
-                            const TextStyle(color: Colors.white, fontSize: 11),
-                      ),
-                    ],
+                        MouseRegion(
+                          onEnter: (_) => newAlbumViewModel
+                              .changeSelectedOption(selectedOption.review),
+                          child: Text(
+                            comment,
+                            style: const TextStyle(
+                                color: Colors.white, fontSize: 11),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               )
